c1832cd1dbdf52122f587daad201b062e5461494
Людок <skobeleva.ludmila@yandex.ru>
2023-01-06 16:41:56 +0300

    первый коммит

--- src/main/java/com/example/lesson4/Lesson4Application.java
+++ src/main/java/com/example/lesson4/Lesson4Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Lesson4Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Lesson4Application.class, args);
     }
 
