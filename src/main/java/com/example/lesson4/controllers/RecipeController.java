f4a8f6139b7b24e8dbbb333f969d2760312774c7
Людок <skobeleva.ludmila@yandex.ru>
2023-01-13 16:14:34 +0300

    Домашнее задание 4

--- src/main/java/com/example/lesson4/controllers/RecipeController.java
+++ src/main/java/com/example/lesson4/controllers/RecipeController.java
@@ -1,11 +1,19 @@
 package com.example.lesson4.controllers;
 
-import com.example.lesson4.model.Recipe;
+import java.util.Map;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.lesson4.model.Recipe;
 import com.example.lesson4.servise.impl.RecipeServiseImpl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.Map;
 
 @RestController
